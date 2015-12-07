e9a43eca8b670dc32e79615e315f99927758c108
Felipe Arenales <fearenales@gmail.com>
2015-12-07 18:03:10 -0200

    Fixing Azkfile

--- docs/Azkfile.js
+++ docs/Azkfile.js
@@ -10,17 +10,21 @@ systems({
     // Steps to execute before running instances
     provision: [
       "npm i",
-      "npm i gitbook-cli -g", // gitbook uses gitbook-cli internally
-      "node node_modules/.bin/gitbook install content",
+      "npm i gitbook-cli", // gitbook-cli has to be installed after gitbook
+      "node_modules/.bin/gitbook versions:link node_modules/gitbook 2.5.0",
+      "node_modules/.bin/gitbook -v 2.5.0 install content",
     ],
     workdir: "/azk/#{manifest.dir}",
     shell: "/bin/bash",
-    command: "node_modules/.bin/gitbook serve --port $HTTP_PORT content",
+    command: [
+      "node_modules/.bin/gitbook versions:link node_modules/gitbook 2.5.0 && ",
+      "node_modules/.bin/gitbook -v 2.5.0 serve --port $HTTP_PORT content"
+    ],
     wait: {"retry": 20, "timeout": 2000},
     mounts: {
-      '/azk/#{manifest.dir}' : path("."),
-      '/azk/CONTRIBUTING.md' : path("../CONTRIBUTING.md"),
-      '/azk/node_modules'    : persistent("node_modules"),
+      '/azk/#{manifest.dir}'              : path("."),
+      '/azk/CONTRIBUTING.md'              : path("../CONTRIBUTING.md"),
+      '/azk/#{manifest.dir}/node_modules' : persistent("node_modules"),
       '/azk/#{manifest.dir}/content/node_modules': persistent("content/node_modules"),
     },
     scalable: {"default": 1},
