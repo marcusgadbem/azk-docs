e3e9e4d3547ccde7093eecc25d8f01f6d37a393a
Marcus Gadbem <mcsgad@gmail.com>
2016-01-21 18:06:58 -0200

    Adds gitbook plugin anchor and its style - modified Azkfile to mount style.css inside content languages - modified Azkfile to make gitbook provision better - modified Azkfile to cache npm packages and build results - added gitbook-plugin-anchors - added anchors style

--- docs/Azkfile.js
+++ docs/Azkfile.js
@@ -10,28 +10,34 @@ systems({
     // Steps to execute before running instances
     provision: [
       "npm i",
-      "npm i gitbook-cli", // gitbook-cli has to be installed after gitbook
-      "node_modules/.bin/gitbook versions:link node_modules/gitbook 2.6.7",
-      "node_modules/.bin/gitbook install content",
+      "gitbook versions:link node_modules/gitbook 2.6.7",
+      "gitbook install content",
     ],
     workdir: "/azk/#{manifest.dir}",
     shell: "/bin/bash",
-    command: "node_modules/.bin/gitbook versions:link node_modules/gitbook 2.6.7 && node_modules/.bin/gitbook serve --port $HTTP_PORT content",
+    command: "gitbook versions:link node_modules/gitbook 2.6.7 && gitbook serve --port $HTTP_PORT content --lrport $LIVERELOAD_PORT",
     wait: {"retry": 20, "timeout": 3000},
     mounts: {
-      '/azk/#{manifest.dir}'              : path("."),
-      '/azk/CONTRIBUTING.md'              : path("../CONTRIBUTING.md"),
-      '/azk/#{manifest.dir}/node_modules' : persistent("node_modules"),
+      '/azk/#{manifest.dir}'               : sync("."),
+      '/azk/CONTRIBUTING.md'               : path("../CONTRIBUTING.md"),
+      '/azk/#{manifest.dir}/content/pt-BR/styles' : sync("./content/common/styles"),
+      '/azk/#{manifest.dir}/content/en/styles' : sync("./content/common/styles"),
+      '/azk/#{manifest.dir}/node_modules'  : persistent("node_modules"),
+      '/azk/#{manifest.dir}/content/_book' : persistent("book"),
       '/azk/#{manifest.dir}/content/node_modules': persistent("content/node_modules"),
+      '/root/.npm': persistent("npm-cache"),
     },
     scalable: {"default": 1},
     http: {
       // docs-azk.
       domains: [ "#{system.name}.#{azk.default_domain}" ]
     },
+    envs: {
+      PATH: "/azk/#{manifest.dir}/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    },
     ports: {
       http:       "5000/tcp",
-      livereload: "35729:35729/tcp",
+      livereload: "35730:35730/tcp",
     },
   },
 });
